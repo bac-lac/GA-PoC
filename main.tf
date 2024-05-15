f00a928bfa1f20eb14d09f1bd81e63eaf3fcd43c
mathieupetersonGC <156801233+mathieupetersonGC@users.noreply.github.com>
2024-05-15 13:20:16 -0400

    Update Security Groups (#2)

--- main.tf
+++ main.tf
@@ -32,7 +32,13 @@ terraform {
 
   resource "aws_security_group" "ga_app_sg" {
     name        = "GA-App-sg"
-    description = "Allow Web traffic to App"
+    description = "Allow traffic between Load Balancer and ECS"
+    vpc_id      = var.VPC_ID
+  }
+
+  resource "aws_security_group" "ga_lb_sg" {
+    name        = "GA-LB-sg"
+    description = "Allow traffic to Laod Balancer"
     vpc_id      = var.VPC_ID
   }
 
@@ -42,5 +48,23 @@ terraform {
     from_port                 = 3306
     protocol                  = "tcp"
     to_port                   = 3306
+    type                      = "ingress"
+  }
+
+  resource "aws_security_group_rule" "allow_tcp_443" {
+    security_group_id         = aws_security_group.ga_app_sg.id
+    source_security_group_id  = aws_security_group.ga_lb_sg.id
+    from_port                 = 443
+    protocol                  = "tcp"
+    to_port                   = 443
+    type                      = "ingress"
+  }
+
+  resource "aws_security_group_rule" "allow_internet" {
+    security_group_id         = aws_security_group.ga_lb_sg.id
+    cidr_ipv4                 = "0.0.0.0/0"
+    from_port                 = 443
+    protocol                  = "tcp"
+    to_port                   = 443
     type                      = "ingress"
   }
