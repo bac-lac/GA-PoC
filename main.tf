8ff38bdc95cc3e0b92e074c2e2a2897341f49a34
cedric guindon <cedric.guindon@gmail.com>
2024-05-16 10:48:41 -0400

    Fix cidr typo

--- main.tf
+++ main.tf
@@ -16,55 +16,55 @@ terraform {
   }
 }
 
-  provider "aws" {
-    region  = "ca-central-1"
-    profile = "DevOps"
-    assume_role {
-      role_arn    = "arn:aws:iam::${var.ACCOUNT}:role/terraform-role"
-    }
+provider "aws" {
+  region  = "ca-central-1"
+  profile = "DevOps"
+  assume_role {
+    role_arn    = "arn:aws:iam::${var.ACCOUNT}:role/terraform-role"
   }
+}
 
-  resource "aws_security_group" "ga_db_sg" {
-    name        = "GA-DB-sg"
-    description = "Allow traffic between ECS and DB"
-    vpc_id      = var.VPC_ID
-  }
+resource "aws_security_group" "ga_db_sg" {
+  name        = "GA-DB-sg"
+  description = "Allow traffic between ECS and DB"
+  vpc_id      = var.VPC_ID
+}
 
-  resource "aws_security_group" "ga_app_sg" {
-    name        = "GA-App-sg"
-    description = "Allow traffic between Load Balancer and ECS"
-    vpc_id      = var.VPC_ID
-  }
+resource "aws_security_group" "ga_app_sg" {
+  name        = "GA-App-sg"
+  description = "Allow traffic between Load Balancer and ECS"
+  vpc_id      = var.VPC_ID
+}
 
-  resource "aws_security_group" "ga_lb_sg" {
-    name        = "GA-LB-sg"
-    description = "Allow traffic to Laod Balancer"
-    vpc_id      = var.VPC_ID
-  }
+resource "aws_security_group" "ga_lb_sg" {
+  name        = "GA-LB-sg"
+  description = "Allow traffic to Laod Balancer"
+  vpc_id      = var.VPC_ID
+}
 
-  resource "aws_security_group_rule" "allow_tcp_3306" {
-    security_group_id         = aws_security_group.ga_db_sg.id
-    source_security_group_id  = aws_security_group.ga_app_sg.id
-    from_port                 = 3306
-    protocol                  = "tcp"
-    to_port                   = 3306
-    type                      = "ingress"
-  }
+resource "aws_security_group_rule" "allow_tcp_3306" {
+  security_group_id         = aws_security_group.ga_db_sg.id
+  source_security_group_id  = aws_security_group.ga_app_sg.id
+  from_port                 = 3306
+  protocol                  = "tcp"
+  to_port                   = 3306
+  type                      = "ingress"
+}
 
-  resource "aws_security_group_rule" "allow_tcp_443" {
-    security_group_id         = aws_security_group.ga_app_sg.id
-    source_security_group_id  = aws_security_group.ga_lb_sg.id
-    from_port                 = 443
-    protocol                  = "tcp"
-    to_port                   = 443
-    type                      = "ingress"
-  }
+resource "aws_security_group_rule" "allow_tcp_443" {
+  security_group_id         = aws_security_group.ga_app_sg.id
+  source_security_group_id  = aws_security_group.ga_lb_sg.id
+  from_port                 = 443
+  protocol                  = "tcp"
+  to_port                   = 443
+  type                      = "ingress"
+}
 
-  resource "aws_security_group_rule" "allow_internet" {
-    security_group_id         = aws_security_group.ga_lb_sg.id
-    cidr_ipv4                 = "0.0.0.0/0"
-    from_port                 = 443
-    protocol                  = "tcp"
-    to_port                   = 443
-    type                      = "ingress"
-  }
+resource "aws_security_group_rule" "allow_internet" {
+  security_group_id         = aws_security_group.ga_lb_sg.id
+  cidr_blocks               = ["0.0.0.0/0"]
+  from_port                 = 443
+  protocol                  = "tcp"
+  to_port                   = 443
+  type                      = "ingress"
+}
