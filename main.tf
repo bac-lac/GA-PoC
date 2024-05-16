f6dfa8297e6aa7634c273fca0f58eb4b90850736
cedric guindon <cedric.guindon@gmail.com>
2024-05-16 14:06:40 -0400

    Add dynamic variable based ot branch name

--- main.tf
+++ main.tf
@@ -21,19 +21,19 @@ provider "aws" {
 }
 
 resource "aws_security_group" "ga_db_sg" {
-  name        = "GA-DB-sg"
+  name        = "GA-DB-${var.BRANCH_NAME}-sg"
   description = "Allow traffic between ECS and DB"
   vpc_id      = var.VPC_ID
 }
 
 resource "aws_security_group" "ga_app_sg" {
-  name        = "GA-App-sg"
+  name        = "GA-App-${var.BRANCH_NAME}-sg"
   description = "Allow traffic between Load Balancer and ECS"
   vpc_id      = var.VPC_ID
 }
 
 resource "aws_security_group" "ga_lb_sg" {
-  name        = "GA-LB-sg"
+  name        = "GA-LB-${var.BRANCH_NAME}-sg"
   description = "Allow traffic to Laod Balancer"
   vpc_id      = var.VPC_ID
 }
