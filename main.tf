390ed5583f1a44455038dc62b22cfb1947211d93
mathieupetersonGC <156801233+mathieupetersonGC@users.noreply.github.com>
2024-05-17 09:43:51 -0400

    Add tag name for security group (#17)
    * Add tag name for security group
    
    * Fix typo

--- main.tf
+++ main.tf
@@ -24,18 +24,27 @@ resource "aws_security_group" "ga_db_sg" {
   name        = "GA-DB-${var.BRANCH_NAME}-sg"
   description = "Allow traffic between ECS and DB"
   vpc_id      = var.VPC_ID
+  tags = {
+    Name = "GA-DB-${var.BRANCH_NAME}-sg"
+  }
 }
 
 resource "aws_security_group" "ga_app_sg" {
   name        = "GA-App-${var.BRANCH_NAME}-sg"
   description = "Allow traffic between Load Balancer and ECS"
   vpc_id      = var.VPC_ID
+  tags = {
+    Name = "GA-App-${var.BRANCH_NAME}-sg"
+  }
 }
 
 resource "aws_security_group" "ga_lb_sg" {
   name        = "GA-LB-${var.BRANCH_NAME}-sg"
-  description = "Allow traffic to Laod Balancer"
+  description = "Allow traffic to Load Balancer"
   vpc_id      = var.VPC_ID
+  tags = {
+    Name = "GA-LB-${var.BRANCH_NAME}-sg"
+  }
 }
 
 resource "aws_security_group_rule" "allow_tcp_3306" {
