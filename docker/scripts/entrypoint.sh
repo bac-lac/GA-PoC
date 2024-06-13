9b475474158e4645d9a5ac5dfa29c1f978d00815
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2024-06-13 14:27:27 -0400

    Update ls

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -98,10 +98,9 @@ ln -s "${shareconfig_folder}"/pesit.xml "${config_folder}"/pesit.xml
 ln -s "${shareconfig_folder}"/security.xml "${config_folder}"/security.xml
 ln -s "${shareconfig_folder}"/sftp.xml "${config_folder}"/sftp.xml
 
-echo "ls -la /etc/HelpSystems/GoAnywhere/userdata"
-ls -la /etc/HelpSystems/GoAnywhere/userdata
-echo "ls -la ${program_folder}/userdata"
-ls -la ${program_folder}/userdata
+
+echo "ls -la ${program_folder}/userdata/logs"
+ls -la ${program_folder}/userdata/logs
 
 JVM='1024'
 if [ -n "$JAVA_MAX_MEMORY" ]; then
