bf4fd87bddc5dbdc48f14672773f1264d4f4b795
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2024-06-17 11:15:37 -0400

    Refactor build

---
+++ docker/scripts/init.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+#######################################
+# Main function
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function main() {
+    echo "Main function"
+
+    copy_filesystem
+    configure_propeties
+    start_app
+
+    echo "Main completed"
+}
+
+#######################################
+# Copy filesystem to mount drive if it doesn't exists.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function copy_filesystem() {
+    echo "Copy filesystem"
+
+    cp -rn /tmp/userdata/ /opt/HelpSystems/GoAnywhere/userdata/
+    cp -rn /tmp/updrager/ /opt/HelpSystems/GoAnywhere/upgrader/
+    cp -rn /tmp/config/ /etc/HelpSystems/GoAnywhere/config/
+    cp -rn /tmp/tomcat/ /etc/HelpSystems/GoAnywhere/tomcat/
+    cp -rn /tmp/logs/ /opt/HelpSystems/GoAnywhere/tomcat/logs/
+    cp -rn /tmp/custom/ /opt/HelpSystems/GoAnywhere/ghttpsroot/custom/
+
+    echo "Copy filesystem completed"
+}
+
+#######################################
+# Configure properties files
+# Globals:
+#   CLUSTER_PORT
+#   DB_PASSWORD
+#   DB_USERNAME
+#   DB_URL
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function configure_propeties() {
+    echo "Configure properties"
+
+    # Variables.
+    config_folder="/etc/HelpSystems/GoAnywhere/config"
+    program_folder="/opt/HelpSystems/GoAnywhere"
+    shareconfig_folder="/etc/HelpSystems/GoAnywhere/sharedconfig"
+    tomcat_folder="/etc/HelpSystems/GoAnywhere/tomcat"
+    entrypoint="/usr/bin/entrypoint.sh"
+
+    cd "${program_folder}"
+
+    # Remove update logic in the entrypoint
+    sed -i 'echo "Updating default database location..."' "${entrypoint}"
+    sed -i "sed -i 's|/usr/local/HelpSystems/GoAnywhere|/opt/HelpSystems/GoAnywhere|g' /etc/HelpSystems/GoAnywhere/config/database.xml" "${entrypoint}"
+    
+    # Update the cluster logic in the entrypoint.
+    sed -i "s|clusterBindPort\">.*<|clusterBindPort\">$CLUSTER_PORT<|g" "${entrypoint}"
+
+
+    echo "cat ${entrypoint}"
+    cat "${entrypoint}"
+
+
+    # Copy temporary files to their default location.
+    cp /tmp/sharedconfig/*.xml "${shareconfig_folder}"
+    cp -R /tmp/sharedconfig/conf/ "${tomcat_folder}"
+    cp /tmp/sharedconfig/cluster.xml "${config_folder}"
+    
+    # Update the file database.xml with the correct values.
+    sed -i "s|password\">.*<|password\">$DB_PASSWORD<|g" "${shareconfig_folder}"/database.xml
+    sed -i "s|username\">.*<|username\">$DB_USERNAME<|g" "${shareconfig_folder}"/database.xml
+    sed -i "s|url\">.*<|url\">$DB_URL<|g" "${shareconfig_folder}"/database.xml
+
+    # Creating symbolic link for application configuration files.
+    cd "${config_folder}"
+    cp cluster.xml /tmp/cluster.xml
+    rm -rf *
+    cp /tmp/cluster.xml .
+    ln -s "${shareconfig_folder}"/database.xml "${config_folder}"/database.xml
+    ln -s "${shareconfig_folder}"/agent.xml "${config_folder}"/agent.xml
+    ln -s "${shareconfig_folder}"/ftp.xml "${config_folder}"/ftp.xml
+    ln -s "${shareconfig_folder}"/ftps.xml "${config_folder}"/ftps.xml
+    ln -s "${shareconfig_folder}"/gateway.xml "${config_folder}"/gateway.xml
+    ln -s "${shareconfig_folder}"/gofast.xml "${config_folder}"/gofast.xml
+    ln -s "${shareconfig_folder}"/https.xml "${config_folder}"/https.xml
+    ln -s "${shareconfig_folder}"/log4j2.xml "${config_folder}"/log4j2.xml
+    ln -s "${shareconfig_folder}"/pesit.xml "${config_folder}"/pesit.xml
+    ln -s "${shareconfig_folder}"/security.xml "${config_folder}"/security.xml
+    ln -s "${shareconfig_folder}"/sftp.xml "${config_folder}"/sftp.xml
+
+    echo "Configure properties completed"
+}
+
+#######################################
+# Start application
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function start_app() {
+    echo "Start application"
+
+    exec /usr/bin/entrypoint.sh
+
+}
+
+main 
