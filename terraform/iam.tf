64f33171ac676fb5b04081b829f7cda592ee24c0
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2024-07-09 15:23:14 -0400

    Add new file

---
+++ terraform/iam.tf
@@ -0,0 +1,19 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["monitoring.rds.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "ga_rds_monitoring_role" {
+  name                = "ga_rds_monitoring_role-${var.BRANCH_NAME}"
+  description         = "Provides access to Cloudwatch for RDS Enhanced Monitoring"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"]
+}
