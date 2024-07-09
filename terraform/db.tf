d69edd99a8f1b4661aab4d6e4f13d2b9e40945c5
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2024-07-09 08:10:22 -0400

    Fix 129

--- terraform/db.tf
+++ terraform/db.tf
@@ -4,17 +4,18 @@ resource "aws_db_subnet_group" "data" {
 }
 
 resource "aws_db_instance" "ga_mysql" {
-  allocated_storage         = 20
-  db_name                   = format("GA%s", replace("${var.BRANCH_NAME}", "-", ""))
-  engine                    = "mysql"
-  engine_version            = "8.0.35"
-  instance_class            = "db.t3.micro"
-  username                  = var.ADMIN_DB_USERNAME
-  password                  = var.ADMIN_DB_PASSWORD
-  parameter_group_name      = "default.mysql8.0"
-  skip_final_snapshot       = true
-  storage_encrypted         = true
-  vpc_security_group_ids    = [data.aws_security_group.data.id]
-  db_subnet_group_name      = aws_db_subnet_group.data.name
-  monitoring_interval       = 5
+  allocated_storage               = 20
+  db_name                         = format("GA%s", replace("${var.BRANCH_NAME}", "-", ""))
+  engine                          = "mysql"
+  engine_version                  = "8.0.35"
+  instance_class                  = "db.t3.micro"
+  username                        = var.ADMIN_DB_USERNAME
+  password                        = var.ADMIN_DB_PASSWORD
+  parameter_group_name            = "default.mysql8.0"
+  skip_final_snapshot             = true
+  storage_encrypted               = true
+  vpc_security_group_ids          = [data.aws_security_group.data.id]
+  db_subnet_group_name            = aws_db_subnet_group.data.name
+  #monitoring_interval            = 5  -> need monitoring_role_arn 
+  enabled_cloudwatch_logs_exports = ["general", "error", "slowquery"]
 }
