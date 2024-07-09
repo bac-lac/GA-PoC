e4f83fa5503ce09fd7ae7aabce36253cdf4c5b27
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2024-07-09 14:01:52 -0400

    Fix 118

--- terraform/db.tf
+++ terraform/db.tf
@@ -16,10 +16,32 @@ resource "aws_db_instance" "ga_mysql" {
   storage_encrypted               = true
   vpc_security_group_ids          = [data.aws_security_group.data.id]
   db_subnet_group_name            = aws_db_subnet_group.data.name
-  #monitoring_interval            = 5  -> need monitoring_role_arn 
   enabled_cloudwatch_logs_exports = ["general", "error", "slowquery"]
   backup_retention_period         = 35
   performance_insights_enabled    = true
   copy_tags_to_snapshot           = true
   apply_immediately               = true
+  identifier                      = "ee-instance-demo"
+  monitoring_interval             = 5
+  monitoring_role_arn             = aws_iam_role.ga_rds_role.arn
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "ga_rds_monitoring_role" {
+  name                = "ga_rds_monitoring_role-${var.BRANCH_NAME}"
+  description         = "Provides access to Cloudwatch for RDS Enhanced Monitoring"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"]
 }
