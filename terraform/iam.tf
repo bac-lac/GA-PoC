293cb3b0505d1d8881a28d1103b2a34f44d6d4a9
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2024-07-10 13:23:12 -0400

    Update based on review

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -14,6 +14,6 @@ data "aws_iam_policy_document" "assume_role" {
 resource "aws_iam_role" "ga_rds_monitoring_role" {
   name                = "ga_rds_monitoring_role-${var.BRANCH_NAME}"
   description         = "Provides access to Cloudwatch for RDS Enhanced Monitoring"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  assume_role_policy  = data.aws_iam_policy_document.assume_role.json
   managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"]
 }
