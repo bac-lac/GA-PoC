9b06022897d7553f084b2bb2aafe6be9dc624cf2
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2024-07-15 10:22:29 -0400

    Fix variable

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -102,7 +102,7 @@ resource "aws_ecs_task_definition" "ga_task_definition" {
     }
   }
   container_definitions = templatefile("task-definitions/mft1.json", {
-                                        ECR_IMAGE       = var.ECR_IMAGE
+                                        ECR_IMAGE       = ${var.ECR_IMAGE}
                                       })
 }
 
