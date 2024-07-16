6f0e43dd29cec58bbebf69ef4b3239d0777e5b94
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2024-07-16 15:08:01 -0400

    Update entrypoint

--- docker/scripts/init.sh
+++ docker/scripts/init.sh
@@ -181,7 +181,9 @@ function configure() {
 
     # Remove "update default database location" in the entrypoint
     echo "Update entrypoint"
-    sed -i '10,15d' /temp/entrypoint.sh 
+    sed -i '10,15d' /temp/entrypoint.sh
+    sed -i "s|$HOSTNAME|$SYSTEM_NAME|g" /temp/entrypoint.sh
+    cat /temp/entrypoint.sh
 
     # Update the file database.xml with the correct values.
     echo "Update database config"
