403c867bb8936e67a09c3b66d5d519406c2e49be
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2024-07-16 14:33:55 -0400

    Rollback

--- docker/scripts/init.sh
+++ docker/scripts/init.sh
@@ -208,7 +208,7 @@ function configure() {
     ln -s "${shareconfig_folder}"/pesit.xml "${config_folder}"/pesit.xml
     ln -s "${shareconfig_folder}"/security.xml "${config_folder}"/security.xml
     ln -s "${shareconfig_folder}"/sftp.xml "${config_folder}"/sftp.xml
-    
+
 }
 
 #######################################
