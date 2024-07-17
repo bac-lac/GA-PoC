6e0f5fc41a82534da45befe61cd4eceef0aa7d3e
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2024-07-17 10:34:38 -0400

    Create role for ECS

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "assume_role" {
+data "aws_iam_policy_document" "ga_rds_monitoring_assume_role" {
   statement {
     effect = "Allow"
 
@@ -14,6 +14,26 @@ data "aws_iam_policy_document" "assume_role" {
 resource "aws_iam_role" "ga_rds_monitoring_role" {
   name                = "ga_rds_monitoring_role-${var.BRANCH_NAME}"
   description         = "Provides access to Cloudwatch for RDS Enhanced Monitoring"
-  assume_role_policy  = data.aws_iam_policy_document.assume_role.json
+  assume_role_policy  = data.aws_iam_policy_document.ga_rds_monitoring_assume_role.json
   managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"]
+}
+
+data "aws_iam_policy_document" "ga_ecs_role_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "ga_ecs_role" {
+  name                = "ga_ecs_role-${var.BRANCH_NAME}"
+  description         = "Provides access to other AWS service resources that are required to run Amazon ECS tasks"
+  assume_role_policy  = data.aws_iam_policy_document.ga_ecs_role_assume_role.json
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"]
 }
