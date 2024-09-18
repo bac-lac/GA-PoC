bb40eb3ca9f68ea59696e1c51592fbd54c8e6f1d
cedric guindon <cedric.guindon@gmail.com>
2024-09-18 10:48:41 -0400

    Update troubleshooting

--- docker/scripts/init.sh
+++ docker/scripts/init.sh
@@ -210,6 +210,11 @@ function configure() {
     ln -s "${shareconfig_folder}"/security.xml "${config_folder}"/security.xml
     ln -s "${shareconfig_folder}"/sftp.xml "${config_folder}"/sftp.xml
 
+
+    echo "*****BEGIN cluster.xml*****"
+    cat "${config_folder}/cluster.xml"
+    echo "*****END cluster.xml*****"
+
 }
 
 #######################################
@@ -226,11 +231,6 @@ function start() {
     echo "Start application"
 
     exec /temp/entrypoint.sh
-
-    echo "*****BEGIN cluster.xml*****"
-    cat "${config_folder}/cluster.xml"
-    echo "*****END cluster.xml*****"
-
 }
 
 main 
