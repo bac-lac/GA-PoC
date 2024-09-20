1ba42417fda67588df746672acae48272bd15671
cedric guindon <cedric.guindon@gmail.com>
2024-09-20 09:22:14 -0400

    Added resource

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -43,7 +43,7 @@ data "aws_iam_policy_document" "ga_ecs_task_role_inline_policy" {
   statement {
     effect    = "Allow"
     actions   = ["ecs:ExecuteCommand","ecs:DescribeTasks","ssmmessages:CreateDataChannel","ssmmessages:OpenDataChannel","ssmmessages:OpenControlChannel","ssmmessages:CreateControlChannel"]
-    resources = ["arn:aws:ecs:ca-central-1:${var.ACCOUNT}:cluster/*","arn:aws:ecs:ca-central-1:${var.ACCOUNT}:task/${aws_ecs_cluster.ga_cluster.name}/*"]
+    resources = ["arn:aws:ecs:ca-central-1:${var.ACCOUNT}:cluster/*","arn:aws:ecs:ca-central-1:${var.ACCOUNT}:task/${aws_ecs_cluster.ga_cluster.name}/*","arn:aws:ssmmessages:ca-central-1:${var.ACCOUNT}:*"]
   }
 }
 
