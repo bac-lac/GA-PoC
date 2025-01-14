d7fe4013a3c730dc990c0d8b28b23973f75fec45
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2025-01-14 10:29:31 -0500

    Remove rule

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -7,32 +7,12 @@ data "aws_lb_listener" "https" {
   port              = 443
 }
 
-data "aws_lb_listener" "http" {
-  load_balancer_arn = data.aws_lb.core_lb.arn
-  port              = 80
-}
-
 resource "aws_lb_listener_rule" "https_rule" {
   listener_arn        = data.aws_lb_listener.https.arn
 
   action {
     type              = "forward"
     target_group_arn  = aws_lb_target_group.ga_tg_8001.arn
-  }
-
-  condition {
-    host_header {
-      values          = [var.BRANCH_NAME == "main" ? "${var.ENV}.ga.bac-lac.ca" : "${var.BRANCH_NAME}.dev.ga.bac-lac.ca"]
-    }
-  }
-}
-
-resource "aws_lb_listener_rule" "http_rule" {
-  listener_arn        = data.aws_lb_listener.http.arn
-
-  action {
-    type              = "forward"
-    target_group_arn  = aws_lb_target_group.ga_tg_8000.arn
   }
 
   condition {
