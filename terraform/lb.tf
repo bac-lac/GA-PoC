925f89d821d129f4df969021b48ded1681b08e6b
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2025-01-22 15:55:29 -0500

    Add protocol

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -81,6 +81,7 @@ data "aws_lb" "dev_nlb"{
 resource "aws_lb_listener" "sftp" {
   load_balancer_arn = data.aws_lb.dev_nlb.arn
   port              = "22"
+  protocol          = "TCP"
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.ga_tg_22.arn
