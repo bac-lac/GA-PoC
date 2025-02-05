8cf4ffb2f967fe8ede13319fe5d9055dafbaa22a
cedric guindon <cedric.guindon@gmail.com>
2025-02-05 07:36:15 -0500

    fix typo

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "ga_s3" {
   bucket = "s3-access-logs-${var.ACCOUNT}-ca-central-1"
-  bucket_prefis = "${var.BRANCH_NAME}"
+  bucket_prefix = "${var.BRANCH_NAME}"
 
   tags = {
     Name        = "${var.BRANCH_NAME}-s3-access-logs-${var.ACCOUNT}-ca-central-1"
