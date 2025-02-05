2a2811fc21c28eca1ae454f5a879ff06ae2a5c39
cedric guindon <cedric.guindon@gmail.com>
2025-02-05 07:30:53 -0500

    add versioning and publick block

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -8,3 +8,19 @@ resource "aws_s3_bucket" "ga_s3" {
   }
   force_destroy = true
 }
+
+resource "aws_s3_bucket_versioning" "default" {
+  bucket = aws_s3_bucket.ga_s3.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "default" {
+  bucket = aws_s3_bucket.ga_s3.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
