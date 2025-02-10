0da5b136388e47c3c197c79ca2e485cc60e8aa5b
cedric guindon <cedric.guindon@gmail.com>
2025-02-10 14:03:36 -0500

    skip retention and update waf

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,4 +1,3 @@
 resource "aws_cloudwatch_log_group" "aws-waf-logs" {
   name = "aws-waf-logs-${var.BRANCH_NAME}"
-  retention_in_days = 731
 }
--- terraform/waf.tf
+++ terraform/waf.tf
@@ -71,10 +71,6 @@ resource "aws_wafv2_web_acl" "ga_web_acl" {
   }
 }
 
-data "aws_cloudwatch_log_group" "ga_cloudwatch" {
-  name = "aws-waf-logs-alb"
-}
-
 resource "aws_wafv2_web_acl_logging_configuration" "ga_web_acl_logging" {
   log_destination_configs = [aws_cloudwatch_log_group.aws-waf-logs.arn]
   resource_arn            = aws_wafv2_web_acl.ga_web_acl.arn
