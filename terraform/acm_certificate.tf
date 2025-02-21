f7138765c104f09ff06e7e84ab6714a78b8d7e17
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2025-02-21 08:42:33 -0500

    Add certificate to certificate manager

--- terraform/acm_certificate.tf
+++ terraform/acm_certificate.tf
@@ -1,3 +1,8 @@
 data "aws_acm_certificate" "baclacca" {
   domain   = "bac-lac.ca"
 }
+
+resource "aws_acm_certificate" "cert" {
+  private_key      = "${var.CERT_PRIVATE_KEY}"
+  certificate_body = "${var.CERT_BODY}"
+}
