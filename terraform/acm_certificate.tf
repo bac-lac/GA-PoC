41e1164510c702cca9879cca987bb645ce1c7782
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2025-02-21 08:49:25 -0500

    Fix CKV_AWS_233

--- terraform/acm_certificate.tf
+++ terraform/acm_certificate.tf
@@ -5,4 +5,8 @@ data "aws_acm_certificate" "baclacca" {
 resource "aws_acm_certificate" "cert" {
   private_key      = "${var.CERT_PRIVATE_KEY}"
   certificate_body = "${var.CERT_BODY}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
