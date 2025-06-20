ca37190f5e427ea14b52dd3afc3eba62699082d9
cedric guindon <cedric.guindon@gmail.com>
2025-06-20 14:27:09 -0400

    add memory and drive

--- terraform/workload/cw.tf
+++ terraform/workload/cw.tf
@@ -47,3 +47,43 @@ resource "aws_cloudwatch_metric_alarm" "ga_cw_db_cpu_alarm" {
   treat_missing_data        = "missing"
   alarm_description         = "This metric monitors RDS ${var.BRANCH_NAME} cpu utilization"
 }
+
+resource "aws_cloudwatch_metric_alarm" "ga_cw_db_memory_alarm" {
+  alarm_name                = "MySQL ${var.BRANCH_NAME} High Memory Utilization"
+  comparison_operator       = "LessThanOrEqualToThreshold"
+  alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
+  insufficient_data_actions = []
+  metric_name               = "FreeableMemory"
+  namespace                 = "AWS/RDS"
+  statistic                 = "Minimum"
+  dimensions = {
+    Name    = "DBInstanceIdentifier"
+    Value   = "${var.BRANCH_NAME}"
+  }
+  period                    = 300
+  evaluation_periods        = 1
+  datapoints_to_alarm       = 1
+  threshold                 = 800
+  treat_missing_data        = "missing"
+  alarm_description         = "This metric monitors RDS ${var.BRANCH_NAME} memory utilization"
+}
+
+resource "aws_cloudwatch_metric_alarm" "ga_cw_db_drive_alarm" {
+  alarm_name                = "MySQL ${var.BRANCH_NAME} drive usage reaching 90%"
+  comparison_operator       = "LessThanThreshold"
+  alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
+  insufficient_data_actions = []
+  metric_name               = "FreeStorageSpace"
+  namespace                 = "AWS/RDS"
+  statistic                 = "Minimum"
+  dimensions = {
+    Name    = "DBInstanceIdentifier"
+    Value   = "${var.BRANCH_NAME}"
+  }
+  period                    = 60
+  evaluation_periods        = 1
+  datapoints_to_alarm       = 1
+  threshold                 = 20
+  treat_missing_data        = "missing"
+  alarm_description         = "This metric monitors RDS ${var.BRANCH_NAME} drive usage reaching 90%"
+}
