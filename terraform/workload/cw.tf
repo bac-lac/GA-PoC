ba68e11e08c06cae722a346047d27d7b7bf6064e
cedric guindon <cedric.guindon@gmail.com>
2025-06-20 13:38:13 -0400

    add branch name

--- terraform/workload/cw.tf
+++ terraform/workload/cw.tf
@@ -29,7 +29,7 @@ resource "aws_sns_topic_subscription" "ga_sns_topic_subscription" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "ga_cw_db_cpu_alarm" {
-  alarm_name                = "MySQL High CPU Utilization"
+  alarm_name                = "MySQL ${var.BRANCH_NAME} High CPU Utilization"
   comparison_operator       = "GreaterThanThreshold"
   alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
   insufficient_data_actions = []
@@ -38,12 +38,12 @@ resource "aws_cloudwatch_metric_alarm" "ga_cw_db_cpu_alarm" {
   statistic                 = "Maximum"
   dimensions = {
     Name    = "DBInstanceIdentifier"
-    Value   = "issue-198"
+    Value   = "${var.BRANCH_NAME}"
   }
   period                    = 60
   evaluation_periods        = 1
   datapoints_to_alarm       = 1
   threshold                 = 80
   treat_missing_data        = missing
-  alarm_description         = "This metric monitors RDS cpu utilization"
+  alarm_description         = "This metric monitors RDS ${var.BRANCH_NAME} cpu utilization"
 }
