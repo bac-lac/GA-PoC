1dfb7d48bf46146145ad9ca34d090678960c13e7
cedric guindon <cedric.guindon@gmail.com>
2025-07-25 07:52:16 -0400

    update alarm name

--- terraform/workload/cloudwatch.tf
+++ terraform/workload/cloudwatch.tf
@@ -139,7 +139,7 @@ resource "aws_cloudwatch_metric_alarm" "ga_cw_ecs_drive_alarm" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "ga_cw_nlb_22_alarm" {
-  alarm_name                = "NLB port 22 ${var.BRANCH_NAME} Healthy Host"
+  alarm_name                = "NLB port 22 ${var.BRANCH_NAME} unhealthy host"
   comparison_operator       = "LessThanThreshold"
   alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
   insufficient_data_actions = []
@@ -152,11 +152,11 @@ resource "aws_cloudwatch_metric_alarm" "ga_cw_nlb_22_alarm" {
   datapoints_to_alarm       = 5
   threshold                 = 1
   treat_missing_data        = "missing"
-  alarm_description         = "This metric monitors NLB port 22 ${var.BRANCH_NAME} Healthy Host"
+  alarm_description         = "This metric monitors NLB port 22 ${var.BRANCH_NAME} health"
 }
 
 resource "aws_cloudwatch_metric_alarm" "ga_cw_alb_443_alarm" {
-  alarm_name                = "ALB port 443 ${var.BRANCH_NAME} Healthy Host"
+  alarm_name                = "ALB port 443 ${var.BRANCH_NAME} unhealthy host"
   comparison_operator       = "LessThanThreshold"
   alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
   insufficient_data_actions = []
@@ -169,11 +169,11 @@ resource "aws_cloudwatch_metric_alarm" "ga_cw_alb_443_alarm" {
   datapoints_to_alarm       = 5
   threshold                 = 1
   treat_missing_data        = "missing"
-  alarm_description         = "This metric monitors ALB port 443 ${var.BRANCH_NAME} Healthy Host"
+  alarm_description         = "This metric monitors ALB port 443 ${var.BRANCH_NAME} health"
 }
 
 resource "aws_cloudwatch_metric_alarm" "ga_cw_alb_8443_alarm" {
-  alarm_name                = "ALB port 8443 ${var.BRANCH_NAME} Healthy Host"
+  alarm_name                = "ALB port 8443 ${var.BRANCH_NAME} unhealthy host"
   comparison_operator       = "LessThanThreshold"
   alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
   insufficient_data_actions = []
@@ -186,5 +186,5 @@ resource "aws_cloudwatch_metric_alarm" "ga_cw_alb_8443_alarm" {
   datapoints_to_alarm       = 5
   threshold                 = 1
   treat_missing_data        = "missing"
-  alarm_description         = "This metric monitors ALB port 8443 ${var.BRANCH_NAME} Healthy Host"
+  alarm_description         = "This metric monitors ALB port 8443 ${var.BRANCH_NAME} health"
 }
