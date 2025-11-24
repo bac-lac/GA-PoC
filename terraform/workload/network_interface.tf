df5550f842380109abcba9b8739d2f37d76eb2e4
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2025-11-24 14:25:52 -0500

    Test resource tag

--- terraform/workload/network_interface.tf
+++ terraform/workload/network_interface.tf
@@ -1,8 +1,6 @@
-resource "aws_network_interface" "test" {
+resource "aws_resource_tag" "tag_eni" {
   for_each = data.aws_network_interfaces.ni.ids
-
-
-  tags = {
-    TESTTAG = each.key
-  }
+  resource_id = each.value
+  key         = each.key
+  value       = each.value
 }
