aa08e00a5d4b4ad7aa3815a33dd0e6eadb41edb3
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2025-11-24 15:14:58 -0500

    Add toset

--- terraform/workload/network_interface.tf
+++ terraform/workload/network_interface.tf
@@ -1,5 +1,5 @@
 resource "aws_ec2_tag" "tag_eni" {
-  for_each = data.aws_network_interfaces.ni.ids
+  for_each = toset(data.aws_network_interfaces.ni.ids)
   resource_id = each.value
   key         = each.key
   value       = each.value
