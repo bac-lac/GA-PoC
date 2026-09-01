aeccb6e3ee1e54500031234a44ead70cddde6efd
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2026-09-01 09:46:49 -0400

    Remove core from repo

--- terraform/workload/lb.tf
+++ terraform/workload/lb.tf
@@ -1,53 +1,74 @@
 data "aws_lb" "ga_alb"{
-  name = "${var.ALB_NAME}"
+  name = "ga-alb-${var.ENV}"
 }
 
 resource "aws_lb_listener" "https" {
   load_balancer_arn   = data.aws_lb.ga_alb.arn
   port                = "443"
   protocol            = "HTTPS"
-  ssl_policy          = "ELBSecurityPolicy-FS-1-2-Res-2019-08"
+  ssl_policy          = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
   certificate_arn     = aws_acm_certificate.baclacgcca.arn
   default_action {
     type = "fixed-response"
 
     fixed_response {
       content_type = "text/plain"
-      message_body = "Default response content"
-      status_code  = "200"
+      message_body = "Invalid host header."
+      status_code  = "400"
     }
   }
 }
 
-resource "aws_lb_listener_rule" "admin_rule" {
+resource "aws_lb_listener_rule" "admin_portal_rule" {
   listener_arn        = aws_lb_listener.https.arn
   action {
     type              = "forward"
-    target_group_arn  = aws_lb_target_group.ga_tg_443.arn
+    target_group_arn  = aws_lb_target_group.ga_tg_8001.arn
   }
   condition {
     host_header {
-      values          = [var.BRANCH_NAME == "main" ? "goanywhere-${var.ENV}.bac-lac.gc.ca" : "${var.BRANCH_NAME}.goanywhere-dev.bac-lac.gc.ca"]
+      values          = [upper(var.ENV) == "PROD" ? "goanywhere.bac-lac.gc.ca" : "goanywhere-${var.ENV}.bac-lac.gc.ca"]
     }
   }
   tags = {
-    Name = "Admin-${var.BRANCH_ENV}"
+    Name = "Admin-${var.ENV}"
   }
 }
 
-resource "aws_lb_listener_rule" "admin_internal_rule" {
+resource "aws_lb_listener_rule" "internal_admin_portal_rule" {
   listener_arn        = aws_lb_listener.https.arn
   action {
     type              = "forward"
-    target_group_arn  = aws_lb_target_group.ga_tg_443.arn
+    target_group_arn  = aws_lb_target_group.ga_tg_8001.arn
   }
   condition {
     host_header {
-      values          = [var.BRANCH_NAME == "main" ? "ga-${var.ENV}-internal.bac-lac.gc.ca" : "${var.BRANCH_NAME}.ga-dev-internal.bac-lac.gc.ca"]
+      values          = [upper(var.ENV) == "PROD" ? "ga-internal.bac-lac.gc.ca" : "ga-${var.ENV}-internal.bac-lac.gc.ca"]
     }
   }
   tags = {
-    Name = "Admin-Internal-${var.BRANCH_ENV}"
+    Name = "Internal-Admin-${var.ENV}"
+  }
+}
+
+resource "aws_lb_target_group" "ga_tg_8001" {
+  name        = "ga-tg-${var.ENV}-8001"
+  port        = 8001
+  protocol    = "HTTPS"
+  target_type = "ip"
+  vpc_id      = data.aws_vpc.vpc.id
+  health_check {
+    path      = "/"
+    matcher   = "200,302"
+    port      = 8001
+    protocol  = "HTTPS"
+  }
+  stickiness {
+    enabled   = true
+    type      = "lb_cookie"
+  }
+  tags = {
+    Name = "Admin-${var.ENV}"
   }
 }
 
@@ -63,33 +84,12 @@ resource "aws_lb_listener_rule" "web_client_rule" {
     }
   }
   tags = {
-    Name = "Web-Client-${var.BRANCH_ENV}"
-  }
-}
-
-resource "aws_lb_target_group" "ga_tg_443" {
-  name        = "ga-tg-${var.BRANCH_ENV}-443"
-  port        = 443
-  protocol    = "HTTPS"
-  target_type = "ip"
-  vpc_id      = data.aws_vpc.vpc.id
-  health_check {
-    path      = "/"
-    matcher   = "200,302"
-    port      = 8001
-    protocol  = "HTTPS"
-  }
-  stickiness {
-    enabled   = true
-    type      = "lb_cookie"
-  }
-  tags = {
-    Name = "Admin-${var.BRANCH_ENV}"
+    Name = "Web-Client-${var.ENV}"
   }
 }
 
 resource "aws_lb_target_group" "ga_tg_8443" {
-  name        = "ga-tg-${var.BRANCH_ENV}-8443"
+  name        = "ga-tg-${var.ENV}-8443"
   port        = 8443
   protocol    = "HTTPS"
   target_type = "ip"
@@ -97,7 +97,7 @@ resource "aws_lb_target_group" "ga_tg_8443" {
   health_check {
     path      = "/"
     matcher   = "200,302"
-    port      = var.BRANCH_NAME == "main" ? 8443 : 8001
+    port      = 8443
     protocol  = "HTTPS"
   }
   stickiness {
@@ -105,38 +105,39 @@ resource "aws_lb_target_group" "ga_tg_8443" {
     type      = "lb_cookie"
   }
   tags = {
-    Name = "Web-client-${var.BRANCH_ENV}"
+    Name = "Web-client-${var.ENV}"
   }
 }
 
+
 data "aws_lb" "ga_nlb"{
-  name = "${var.NLB_NAME}"
+  name = "ga-nlb-${var.ENV}"
 }
 
 resource "aws_lb_listener" "sftp" {
   load_balancer_arn   = data.aws_lb.ga_nlb.arn
-  port                = "22"
+  port                = "8022"
   protocol            = "TCP"
   default_action {
     type              = "forward"
-    target_group_arn  = aws_lb_target_group.ga_tg_22.arn
+    target_group_arn  = aws_lb_target_group.ga_tg_8022.arn
   }
   tags = {
-    Name = "SFTP-${var.BRANCH_ENV}"
+    Name = "SFTP-${var.ENV}"
   }
 }
 
-resource "aws_lb_target_group" "ga_tg_22" {
-  name        = "ga-tg-${var.BRANCH_ENV}-22"
-  port        = 22
+resource "aws_lb_target_group" "ga_tg_8022" {
+  name        = "ga-tg-${var.ENV}-8022"
+  port        = 8022
   protocol    = "TCP"
   target_type = "ip"
   vpc_id      = data.aws_vpc.vpc.id
   health_check {
-    port      = var.BRANCH_NAME == "main" ? 8022 : 8001
+    port      = 8022
     protocol  = "TCP"
   }
   tags = {
-    Name = "SFTP-${var.BRANCH_ENV}"
+    Name = "SFTP-${var.ENV}"
   }
 }
