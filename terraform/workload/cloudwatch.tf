76a4cba003ba9bf8aeddf9ef69347a59473713e8
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2026-09-02 11:51:00 -0400

    Add missing cloudwatch alerts

--- terraform/workload/cloudwatch.tf
+++ terraform/workload/cloudwatch.tf
@@ -1,13 +1,12 @@
 resource "aws_cloudwatch_metric_alarm" "ga_cw_db_cpu_alarm" {
-  alarm_name                = "MySQL ${var.ENV} High CPU Utilization"
-  comparison_operator       = "GreaterThanThreshold"
+  alarm_name                = "${var.ENV} - MySQL CPU reaching 90%"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
   alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
-  insufficient_data_actions = []
   metric_name               = "CPUUtilization"
   namespace                 = "AWS/RDS"
   statistic                 = "Maximum"
   dimensions = {
-    DBInstanceIdentifier  = aws_db_instance.ga_mysql.identifier
+    DBInstanceIdentifier    = aws_db_instance.ga_mysql.identifier
   }
   period                    = 60
   evaluation_periods        = 5
@@ -18,15 +17,14 @@ resource "aws_cloudwatch_metric_alarm" "ga_cw_db_cpu_alarm" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "ga_cw_db_memory_alarm" {
-  alarm_name                = "MySQL ${var.ENV} High Memory Utilization"
+  alarm_name                = "${var.ENV} - MySQL Memory reaching 90%"
   comparison_operator       = "LessThanOrEqualToThreshold"
   alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
-  insufficient_data_actions = []
   metric_name               = "FreeableMemory"
   namespace                 = "AWS/RDS"
   statistic                 = "Minimum"
   dimensions = {
-    DBInstanceIdentifier  = aws_db_instance.ga_mysql.identifier
+    DBInstanceIdentifier    = aws_db_instance.ga_mysql.identifier
   }
   period                    = 60
   evaluation_periods        = 5
@@ -36,31 +34,29 @@ resource "aws_cloudwatch_metric_alarm" "ga_cw_db_memory_alarm" {
   alarm_description         = "This metric monitors RDS ${var.ENV} memory utilization"
 }
 
-resource "aws_cloudwatch_metric_alarm" "ga_cw_db_drive_alarm" {
-  alarm_name                = "MySQL ${var.ENV} drive usage reaching 90%"
-  comparison_operator       = "LessThanThreshold"
+resource "aws_cloudwatch_metric_alarm" "ga_cw_db_storage_alarm" {
+  alarm_name                = "${var.ENV} - MySQL Storage reaching 90%"
+  comparison_operator       = "LessThanOrEqualToThreshold"
   alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
-  insufficient_data_actions = []
   metric_name               = "FreeStorageSpace"
   namespace                 = "AWS/RDS"
   statistic                 = "Minimum"
   dimensions = {
-    DBInstanceIdentifier  = aws_db_instance.ga_mysql.identifier
+    DBInstanceIdentifier    = aws_db_instance.ga_mysql.identifier
   }
   period                    = 60
   evaluation_periods        = 5
   datapoints_to_alarm       = 5
   threshold                 = floor(aws_db_instance.ga_mysql.allocated_storage * 1024 * 1024 * 1024 * 0.10)
   treat_missing_data        = "missing"
-  alarm_description         = "This metric monitors RDS ${var.ENV} drive usage reaching 90%"
+  alarm_description         = "This metric monitors RDS ${var.ENV} storage utilization"
 }
 
 resource "aws_cloudwatch_metric_alarm" "ga_cw_ecs_cpu_alarm" {
   for_each                  = upper(var.MFT_CLUSTER) == "TRUE" ? toset(["1", "2"]) : toset(["1"])
-  alarm_name                = "MFT-${each.key} ${var.ENV} High CPU Utilization"
-  comparison_operator       = "GreaterThanThreshold"
+  alarm_name                = "${var.ENV} - MFT-${each.key} CPU reaching 90%"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
   alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
-  insufficient_data_actions = []
   metric_name               = "CPUUtilization"
   namespace                 = "AWS/ECS"
   statistic                 = "Maximum"
@@ -75,7 +71,7 @@ resource "aws_cloudwatch_metric_alarm" "ga_cw_ecs_cpu_alarm" {
 
 resource "aws_cloudwatch_metric_alarm" "ga_cw_ecs_memory_alarm" {
   for_each                  = upper(var.MFT_CLUSTER) == "TRUE" ? toset(["1", "2"]) : toset(["1"])
-  alarm_name                = "MFT-${each.key} ${var.ENV} High Memory Utilization"
+  alarm_name                = "${var.ENV} - MFT-${each.key} Memory reaching 90%"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
   insufficient_data_actions = []
@@ -91,17 +87,16 @@ resource "aws_cloudwatch_metric_alarm" "ga_cw_ecs_memory_alarm" {
   alarm_description         = "This metric monitors MFT-${each.key} ${var.ENV} memory utilization"
 }
 
-resource "aws_cloudwatch_metric_alarm" "ga_cw_ecs_drive_alarm" {
+resource "aws_cloudwatch_metric_alarm" "ga_cw_ecs_storage_alarm" {
   for_each                  = upper(var.MFT_CLUSTER) == "TRUE" ? toset(["1", "2"]) : toset(["1"])
-  alarm_name                = "MFT-${each.key} ${var.ENV} drive usage reaching 90%"
+  alarm_name                = "${var.ENV} - MFT-${each.key} Storage reaching 90%"
   comparison_operator       = "GreaterThanThreshold"
   alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
-  insufficient_data_actions = []
   evaluation_periods        = 5
   datapoints_to_alarm       = 5
   threshold                 = 90
   treat_missing_data        = "missing"
-  alarm_description         = "This metric monitors MFT-${each.key} ${var.ENV} drive utilization"
+  alarm_description         = "This metric monitors MFT-${each.key} ${var.ENV} storage utilization"
 
   metric_query {
     id          = "e1"
@@ -138,11 +133,10 @@ resource "aws_cloudwatch_metric_alarm" "ga_cw_ecs_drive_alarm" {
 
 }
 
-resource "aws_cloudwatch_metric_alarm" "ga_cw_nlb_22_alarm" {
-  alarm_name                = "NLB port 22 ${var.ENV} unhealthy host"
+resource "aws_cloudwatch_metric_alarm" "ga_cw_nlb_8022_alarm" {
+  alarm_name                = "${var.ENV} - NLB port 8022 (SFTP) unhealthy host"
   comparison_operator       = "LessThanThreshold"
   alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
-  insufficient_data_actions = []
   metric_name               = "HealthyHostCount"
   namespace                 = "AWS/NetworkELB"
   statistic                 = "Minimum"
@@ -152,14 +146,13 @@ resource "aws_cloudwatch_metric_alarm" "ga_cw_nlb_22_alarm" {
   datapoints_to_alarm       = 5
   threshold                 = 1
   treat_missing_data        = "missing"
-  alarm_description         = "This metric monitors NLB port 22 ${var.ENV} health"
+  alarm_description         = "This metric monitors NLB port 8022 (SFTP) ${var.ENV} health"
 }
 
-resource "aws_cloudwatch_metric_alarm" "ga_cw_alb_443_alarm" {
-  alarm_name                = "ALB port 443 ${var.ENV} unhealthy host"
+resource "aws_cloudwatch_metric_alarm" "ga_cw_alb_8001_alarm" {
+  alarm_name                = "${var.ENV} - ALB port 8001 (admin portal) unhealthy host"
   comparison_operator       = "LessThanThreshold"
   alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
-  insufficient_data_actions = []
   metric_name               = "HealthyHostCount"
   namespace                 = "AWS/ApplicationELB"
   statistic                 = "Minimum"
@@ -169,14 +162,13 @@ resource "aws_cloudwatch_metric_alarm" "ga_cw_alb_443_alarm" {
   datapoints_to_alarm       = 5
   threshold                 = 1
   treat_missing_data        = "missing"
-  alarm_description         = "This metric monitors ALB port 443 ${var.ENV} health"
+  alarm_description         = "This metric monitors ALB port 8001 (admin portal) ${var.ENV} health"
 }
 
 resource "aws_cloudwatch_metric_alarm" "ga_cw_alb_8443_alarm" {
-  alarm_name                = "ALB port 8443 ${var.ENV} unhealthy host"
+  alarm_name                = "${var.ENV} - ALB port 8443 (web client) unhealthy host"
   comparison_operator       = "LessThanThreshold"
   alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
-  insufficient_data_actions = []
   metric_name               = "HealthyHostCount"
   namespace                 = "AWS/ApplicationELB"
   statistic                 = "Minimum"
@@ -186,5 +178,21 @@ resource "aws_cloudwatch_metric_alarm" "ga_cw_alb_8443_alarm" {
   datapoints_to_alarm       = 5
   threshold                 = 1
   treat_missing_data        = "missing"
-  alarm_description         = "This metric monitors ALB port 8443 ${var.ENV} health"
+  alarm_description         = "This metric monitors ALB port 8443 (web client) ${var.ENV} health"
+}
+
+resource "aws_cloudwatch_metric_alarm" "ga_cw_nlb_8009_alarm" {
+  alarm_name                = "${var.ENV} - NLB port 8009 (Agent) unhealthy host"
+  comparison_operator       = "LessThanThreshold"
+  alarm_actions             = [aws_sns_topic.ga_sns_topic.arn]
+  metric_name               = "HealthyHostCount"
+  namespace                 = "AWS/NetworkELB"
+  statistic                 = "Minimum"
+  dimensions                = zipmap(["TargetGroup", "LoadBalancer"], [aws_lb_target_group.ga_tg_8009.arn_suffix, data.aws_lb.ga_nlb.arn_suffix])
+  period                    = 60
+  evaluation_periods        = 5
+  datapoints_to_alarm       = 5
+  threshold                 = upper(var.MFT_CLUSTER) == "TRUE" ? 2 : 1
+  treat_missing_data        = "missing"
+  alarm_description         = "This metric monitors NLB port 8009 (Agent) ${var.ENV} health"
 }
