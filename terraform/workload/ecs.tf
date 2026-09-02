803d7b39a68d09e7b0943f03ea773c8b35f120d1
mathieupetersonGC <156801233+mathieupetersonGC@users.noreply.github.com>
2026-09-02 14:53:44 -0400

    Remove module for ECS (#246)
    * Remove module for ECS
    
    * Add temporary option for pr
    
    * Update count index
    
    * Fix cluster
    
    * Fix task definition count
    
    * Fix mft number
    
    * Fix cluster id
    
    * Rollback to dev

--- terraform/workload/ecs.tf
+++ terraform/workload/ecs.tf
@@ -13,37 +13,89 @@ resource "aws_ecs_cluster_capacity_providers" "ga_cluster_capacity_providers" {
   capacity_providers = ["FARGATE"]
 }
 
-module "ecs_service" {
-  source                      = "./modules/ecs_service/"
-  for_each                    = upper(var.MFT_CLUSTER) == "TRUE" ? toset(["1", "2"]) : toset(["1"])
-  MOD_MFT_NUMBER              = each.key
-  MOD_CLUSTER                 = var.MFT_CLUSTER
-  MOD_FILE_SYSTEM_ID          = aws_efs_file_system.ga_efs.id
-  MOD_GA_AP_USERDATA_ID       = aws_efs_access_point.ga_ap["userdata"].id
-  MOD_GA_AP_SHAREDCONFIG_ID   = aws_efs_access_point.ga_ap["sharedconfig"].id
-  MOD_GA_AP_UPGRADER_ID       = aws_efs_access_point.ga_ap["upgrader${each.key}"].id
-  MOD_GA_AP_CONFIG_ID         = aws_efs_access_point.ga_ap["config${each.key}"].id
-  MOD_GA_AP_TOMCATSERVER_ID   = aws_efs_access_point.ga_ap["tomcatserver${each.key}"].id
-  MOD_GA_AP_TOMCATLOG_ID      = aws_efs_access_point.ga_ap["tomcatlog${each.key}"].id
-  MOD_GA_AP_GHTTPSROOT_ID     = aws_efs_access_point.ga_ap["ghttpsroot${each.key}"].id
-  MOD_DB_ADDRESS              = aws_db_instance.ga_mysql.address
-  MOD_ECS_ROLE_ARN            = aws_iam_role.ga_ecs_role.arn
-  MOD_ECS_TASK_ROLE_ARN       = aws_iam_role.ga_ecs_task_role.arn
-  MOD_ECR_IMAGE               = var.ECR_IMAGE
-  MOD_ENV                     = var.ENV
-  MOD_DB_USERNAME             = var.DB_USERNAME
-  MOD_DB_PASSWORD             = var.DB_PASSWORD
-  MOD_ADMIN_DB_USERNAME       = var.ADMIN_DB_USERNAME
-  MOD_ADMIN_DB_PASSWORD       = var.ADMIN_DB_PASSWORD
-  MOD_FORCE_REFRESH           = var.FORCE_REFRESH
-  MOD_FRESH_INSTALL           = var.FRESH_INSTALL
-  MOD_CLUSTER_ID              = aws_ecs_cluster.ga_cluster.id
-  MOD_SUBNETS                 = data.aws_subnets.app.ids
-  MOD_SECURITY_GROUP_ID       = data.aws_security_group.app.id
-  MOD_TARGET_GROUP_8001_ARN   = aws_lb_target_group.ga_tg_8001.arn
-  MOD_TARGET_GROUP_8009_ARN   = aws_lb_target_group.ga_tg_8009.arn
-  MOD_TARGET_GROUP_8443_ARN   = aws_lb_target_group.ga_tg_8443.arn
-  MOD_TARGET_GROUP_8022_ARN   = aws_lb_target_group.ga_tg_8022.arn
-  MOD_TASK_DEFINITION_CPU     = var.TASK_DEFINITION_CPU
-  MOD_TASK_DEFINITION_MEMORY  = var.TASK_DEFINITION_MEMORY
+resource "aws_ecs_task_definition" "ga_task_definition_mft" {
+  count                     = upper(var.MFT_CLUSTER) == "TRUE" ? 2 : 1
+  family                    = "ga-task-definition-mft${count.index + 1}-${var.ENV}"
+  requires_compatibilities  = ["FARGATE"]
+  network_mode              = "awsvpc"
+  cpu                       = var.TASK_DEFINITION_CPU
+  memory                    = var.TASK_DEFINITION_MEMORY
+  execution_role_arn        = aws_iam_role.ga_ecs_role.arn
+  task_role_arn             = aws_iam_role.ga_ecs_task_role.arn
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+  dynamic "volume" {
+    for_each = tomap({
+      ga_ap_userdata      = aws_efs_access_point.ga_ap["userdata"].id
+      ga_ap_sharedconfig  = aws_efs_access_point.ga_ap["sharedconfig"].id
+      ga_ap_upgrader      = aws_efs_access_point.ga_ap["upgrader${count.index + 1}"].id
+      ga_ap_config        = aws_efs_access_point.ga_ap["config${count.index + 1}"].id
+      ga_ap_tomcatserver  = aws_efs_access_point.ga_ap["tomcatserver${count.index + 1}"].id
+      ga_ap_tomcatlog     = aws_efs_access_point.ga_ap["tomcatlog${count.index + 1}"].id
+      ga_ap_ghttpsroot    = aws_efs_access_point.ga_ap["ghttpsroot${count.index + 1}"].id
+    })
+    content {
+      name = volume.key
+
+      efs_volume_configuration {
+        file_system_id        = aws_efs_file_system.ga_efs.id
+        transit_encryption    = "ENABLED"
+        authorization_config {
+          access_point_id = volume.value
+        }
+      }
+    }
+  }
+  container_definitions = templatefile("task-definitions/mft.tftpl", {
+                                        ECR_IMAGE         = var.ECR_IMAGE,
+                                        DB_USERNAME       = var.DB_USERNAME,
+                                        DB_ADDRESS        = aws_db_instance.ga_mysql.address,
+                                        DB_PASSWORD       = var.DB_PASSWORD,
+                                        ADMIN_DB_USERNAME = var.ADMIN_DB_USERNAME,
+                                        ADMIN_DB_PASSWORD = var.ADMIN_DB_PASSWORD,
+                                        FORCE_REFRESH     = var.FORCE_REFRESH,
+                                        FRESH_INSTALL     = var.FRESH_INSTALL,
+                                        MFT_NUMBER        = count.index + 1,
+                                        MFT_CLUSTER       = var.MFT_CLUSTER
+                                      })
+}
+
+resource "aws_ecs_service" "ga_service_mft" {
+  count                 = upper(var.MFT_CLUSTER) == "TRUE" ? 2 : 1
+  name                  = "ga-service-mft${count.index + 1}-${var.ENV}"
+  cluster               = aws_ecs_cluster.ga_cluster.id
+  task_definition       = aws_ecs_task_definition.ga_task_definition_mft[count.index].arn
+  launch_type           = "FARGATE"
+  platform_version      = "LATEST"
+  force_new_deployment  = true
+  scheduling_strategy   = "REPLICA"
+  desired_count         = 1
+  network_configuration {
+    subnets             = data.aws_subnets.app.ids
+    security_groups     = [data.aws_security_group.app.id]
+    assign_public_ip    = false
+  }
+  load_balancer {
+    target_group_arn    = aws_lb_target_group.ga_tg_8001.arn
+    container_name      = "MFT-${count.index + 1}"
+    container_port      = 8001
+  }
+  load_balancer {
+    target_group_arn    = aws_lb_target_group.ga_tg_8009.arn
+    container_name      = "MFT-${count.index + 1}"
+    container_port      = 8009
+  }
+  load_balancer {
+    target_group_arn    = aws_lb_target_group.ga_tg_8443.arn
+    container_name      = "MFT-${count.index + 1}"
+    container_port      = 8443
+  }
+  load_balancer {
+    target_group_arn    = aws_lb_target_group.ga_tg_8022.arn
+    container_name      = "MFT-${count.index + 1}"
+    container_port      = 8022
+  }
+  enable_execute_command = true
 }
