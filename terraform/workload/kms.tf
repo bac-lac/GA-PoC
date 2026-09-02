62b4fded1ddeab0f655eba9f8ec6969dfacdf3c4
mathieupetersonGC <156801233+mathieupetersonGC@users.noreply.github.com>
2026-09-02 11:21:26 -0400

    Fix code  (#243)
    * Add 'pr' option to deployment environment choices
    
    * Update KMS alias name for EFS
    
    * Update dimensions for CloudWatch metrics
    
    * Add KMS alias data for SNS
    
    * Update KMS key for SNS topic
    
    * Add SNS KMS alias to IAM policy resources
    
    * Update CI/CD workflow for 'pr' environment
    
    * Add allowPublicKeyRetrieval to database URL
    
    * Add 'pr' option to environment choices in cleanup.yml
    
    * Change ECR login condition from 'pr' to 'dev'

--- terraform/workload/kms.tf
+++ terraform/workload/kms.tf
@@ -1,7 +1,7 @@
-data "aws_kms_alias" "ga_kms_main" {
-  name = "alias/main"
+data "aws_kms_alias" "sns" {
+  name = "alias/aws/sns"
 }
 
 data "aws_kms_alias" "efs" {
-  name = "aws/elasticfilesystem"
+  name = "alias/aws/elasticfilesystem"
 }
