5d0db3c936b71c58574a417424da1662ed670e28
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2026-09-10 11:47:44 -0400

    Update creation sym link

--- docker/scripts/init.sh
+++ docker/scripts/init.sh
@@ -192,26 +192,26 @@ function configure() {
         echo "Copy config files to the shared folder"
         cp -Rf /temp/config/*.xml "${shareconfig_folder}"
 
-        # Creating symbolic link for application configuration files.
-        echo "Creating symbolic link for application configuration files"
-        cd "${config_folder}"
-        cp cluster.xml /tmp/cluster.xml
-        rm -rf ./*
-        cp /tmp/cluster.xml .
-        ln -s "${shareconfig_folder}"/agent.xml "${config_folder}"/agent.xml
-        ln -s "${shareconfig_folder}"/database.xml "${config_folder}"/database.xml
-        ln -s "${shareconfig_folder}"/filecatalyst.xml "${config_folder}"/filecatalyst.xml
-        ln -s "${shareconfig_folder}"/ftp.xml "${config_folder}"/ftp.xml
-        ln -s "${shareconfig_folder}"/ftps.xml "${config_folder}"/ftps.xml
-        ln -s "${shareconfig_folder}"/gateway.xml "${config_folder}"/gateway.xml
-        ln -s "${shareconfig_folder}"/gofast.xml "${config_folder}"/gofast.xml
-        ln -s "${shareconfig_folder}"/https.xml "${config_folder}"/https.xml
-        ln -s "${shareconfig_folder}"/log4j2.xml "${config_folder}"/log4j2.xml
-        ln -s "${shareconfig_folder}"/pesit.xml "${config_folder}"/pesit.xml
-        ln -s "${shareconfig_folder}"/security.xml "${config_folder}"/security.xml
-        ln -s "${shareconfig_folder}"/sftp.xml "${config_folder}"/sftp.xml
-
     fi
+
+    # Creating symbolic link for application configuration files.
+    echo "Creating symbolic link for application configuration files"
+    cd "${config_folder}"
+    cp cluster.xml /tmp/cluster.xml
+    rm -rf ./*
+    cp /tmp/cluster.xml .
+    ln -s "${shareconfig_folder}"/agent.xml "${config_folder}"/agent.xml
+    ln -s "${shareconfig_folder}"/database.xml "${config_folder}"/database.xml
+    ln -s "${shareconfig_folder}"/filecatalyst.xml "${config_folder}"/filecatalyst.xml
+    ln -s "${shareconfig_folder}"/ftp.xml "${config_folder}"/ftp.xml
+    ln -s "${shareconfig_folder}"/ftps.xml "${config_folder}"/ftps.xml
+    ln -s "${shareconfig_folder}"/gateway.xml "${config_folder}"/gateway.xml
+    ln -s "${shareconfig_folder}"/gofast.xml "${config_folder}"/gofast.xml
+    ln -s "${shareconfig_folder}"/https.xml "${config_folder}"/https.xml
+    ln -s "${shareconfig_folder}"/log4j2.xml "${config_folder}"/log4j2.xml
+    ln -s "${shareconfig_folder}"/pesit.xml "${config_folder}"/pesit.xml
+    ln -s "${shareconfig_folder}"/security.xml "${config_folder}"/security.xml
+    ln -s "${shareconfig_folder}"/sftp.xml "${config_folder}"/sftp.xml
 
     # Remove "update default ports" in the entrypoint.
     echo "Update entrypoint"
