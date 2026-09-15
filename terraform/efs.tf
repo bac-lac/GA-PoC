1aae67bfb73ef9a535a832e6d4e49d7b076430de
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2026-09-15 09:23:54 -0400

    Update efs access point

--- terraform/efs.tf
+++ terraform/efs.tf
@@ -16,7 +16,20 @@ resource "aws_efs_mount_target" "ga_efs_mount_target" {
 }
 
 resource "aws_efs_access_point" "ga_ap" {
-  for_each = toset(["userdata","upgrader1","config1","tomcatserver1","tomcatlog1","ghttpsroot1","upgrader2","config2","tomcatserver2","tomcatlog2","ghttpsroot2"])
+  for_each = toset(
+    concat(
+      ["userdata"],
+      flatten([
+        for i in range(1, upper(var.MFT_CLUSTER) == "TRUE" ? 2 : 1) : [
+          "upgrader${i}",
+          "config${i}",
+          "tomcatserver${i}",
+          "tomcatlog${i}",
+          "ghttpsroot${i}"
+        ]
+      ])
+    )
+  )
   file_system_id  = aws_efs_file_system.ga_efs.id
   posix_user {
     gid = 992
